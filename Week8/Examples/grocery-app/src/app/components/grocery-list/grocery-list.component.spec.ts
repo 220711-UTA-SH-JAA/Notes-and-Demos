9934126186e6d2d891989517938e436def8b21a1
elmcgill <ethan.mcgill2016@gmail.com>
2022-09-02 10:12:04 -0500

    end of week 8

--- Week8/Examples/grocery-app/src/app/components/grocery-list/grocery-list.component.spec.ts
+++ Week8/Examples/grocery-app/src/app/components/grocery-list/grocery-list.component.spec.ts
@@ -1,6 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { BehaviorSubject } from 'rxjs';
 
 import { GroceryListComponent } from './grocery-list.component';
+import {GrocerylistService} from '../../services/grocerylist.service';
+import {NewItemComponent} from '../new-item/new-item.component';
+import {User} from '../../interfaces/User';
+import { By } from '@angular/platform-browser';
+
+//When we test, similar to Java, we do not want to actually make calls to the api, so we must mock the
+//classes that make those calls, in our case the service class
+class MockGroceryListService {
+  //This has to have the same methods and properties as our real service
+  subject = new BehaviorSubject([
+    {
+      listId: 0,
+      listName: '',
+      items: []
+    }
+  ]);
+
+  //mock our loadLists method
+  loadLists(user:User){
+    return {
+      listId: 1,
+      listName: 'test-list',
+      items: []
+    }
+  }
+
+  //mock our addItem method
+  addItem(listName:string, item:string){
+    return {
+      listId: 1,
+      listName: 'test-list',
+      items:[{
+        itemId: 1, 
+        itemName: item,
+        itemPrice: 1.99
+      }]
+    }
+  }
+}
 
 describe('GroceryListComponent', () => {
   let component: GroceryListComponent;
@@ -8,7 +49,9 @@ describe('GroceryListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GroceryListComponent ]
+      declarations: [ GroceryListComponent, NewItemComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [{provide: GrocerylistService, useClass:MockGroceryListService}]
     })
     .compileComponents();
 
@@ -20,4 +63,27 @@ describe('GroceryListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call addItem() of the grocerylistservice when it receives a new item from the new-item component', () => {
+    //We must first get an instance of our grocerylist service, so we can spy on it
+    let service = fixture.debugElement.injector.get(GrocerylistService);
+
+    //We need to setup a spy, to watch and make sure the correct method is called on the action
+    let serviceSpy = spyOn(service, 'addItem').and.callThrough();
+
+    //We need an instance of the new item component to send the event/object
+    const newItem = fixture.debugElement.query(By.directive(NewItemComponent));
+
+    //Triggers our custom event, emitting the event object with the item, which will then trigger our grocerylist
+    //to call the service
+    newItem.triggerEventHandler('onAddItem', {
+      itemId: 1,
+      itemName: 'test',
+      itemPrice: 9.99
+    });
+
+    fixture.detectChanges();
+    expect(serviceSpy).toHaveBeenCalled();
+  })
+
 });
--- Week8/Examples/grocery-app/src/app/components/new-item/new-item.component.spec.ts
+++ Week8/Examples/grocery-app/src/app/components/new-item/new-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { NewItemComponent } from './new-item.component';
 
@@ -20,4 +21,36 @@ describe('NewItemComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //Describe what the test does in the it function
+  it('should display "Input new item" when error is false', ()=> {
+    //Put the actual logic of the test inside of the callback
+    //Get access to the h2 of this component
+    const h2 = fixture.debugElement.query(By.css('h2')).nativeElement;
+    //We expect the text/html of this h2 to say "Input an new item"
+    expect(h2.innerHTML).toBe("Input a new item");
+  });
+
+  //Test an event that occurs on the component
+  it('should change the valud of error when input is missing', () => {
+    //To start out, the value of error should be false
+    expect(component.error.valueOf()).toBeFalsy();
+
+    //Lets cause the submit to happen
+    component.submitItem();
+
+    //Must look for changes on the component
+    fixture.detectChanges();
+
+    //Now the error value should be updated to false, because the other values on the page were empty
+    expect(component.error.valueOf()).toBeTruthy();
+  });
+
+  it('should display the message "Please input both values" if error is true', () => {
+    component.submitItem();
+    fixture.detectChanges();
+    const h2 = fixture.debugElement.query(By.css('h2')).nativeElement;
+    expect(h2.innerHTML).toBe("Please input both values");
+  })
+
 });
